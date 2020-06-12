4c9442c478f9eb883b33564800295992bcee48ed
Jefferson Eloy <jeffersoneloy08@gmail.com>
2020-06-12 09:49:22 -0300

    :rocket: Added map

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Header, TextHeader, StatusBar } from "./styles";
+import MapView, { Marker, Callout } from "react-native-maps";
+import {
+  requestPermissionsAsync,
+  getCurrentPositionAsync,
+} from "expo-location";
 function Home() {
+  const [region, setRegion] = useState(null);
+
+  useEffect(() => {
+    async function loadPosition() {
+      const { granted } = await requestPermissionsAsync();
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: false,
+        });
+
+        const { latitude, longitude } = coords;
+
+        setRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 10.0,
+          longitudeDelta: 20.0,
+        });
+      }
+    }
+    loadPosition();
+  }, [region]);
+
+  function handleRegionChanged(region) {
+    setRegion(region);
+  }
+
   return (
     <Container>
       <StatusBar barStyle="light-content" backgroundColor="#0f7778" />
@@ -8,6 +40,12 @@ function Home() {
         <TextHeader>COVID - 19</TextHeader>
         <TextHeader>LOGO</TextHeader>
       </Header>
+
+      <MapView
+        onRegionChangeComplete={handleRegionChanged}
+        style={{ flex: 1 }}
+        initialRegion={region}
+      ></MapView>
     </Container>
   );
 }

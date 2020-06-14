ad41b0406f517e00125aa53aa7900d2a1aa8e994
Jefferson Eloy <jeffersoneloy08@gmail.com>
2020-06-14 17:32:50 -0300

    :rocket: Finished adding markers

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,6 +6,8 @@ import {
   StatusBar,
   CalloutContent,
   CalloutText,
+  ImageMarker,
+  ImageHeader,
 } from "./styles";
 
 import MapView, { Marker, Callout } from "react-native-maps";
@@ -13,12 +15,16 @@ import {
   requestPermissionsAsync,
   getCurrentPositionAsync,
 } from "expo-location";
-import { View, Text } from "react-native";
+
 import api from "../../services/api";
+
+import logoImg from "../../assets/logo.png";
+
+import coords from "../../database/states";
 
 function Home() {
   const [region, setRegion] = useState(null);
-  const [states, setStates] = useState("");
+  const [states, setStates] = useState([]);
 
   useEffect(() => {
     async function loadPosition() {
@@ -39,16 +45,46 @@ function Home() {
       }
     }
     loadPosition();
-  }, [region]);
+  }, []);
 
   function handleRegionChanged(region) {
     setRegion(region);
   }
 
   async function loadsCasesInformation() {
-    const response = await api.get(`/brazil/uf/BA`);
+    const getStates = (await api.get("/report/v1")).data;
 
-    setStates(response.data);
+    const states = getStates.data;
+
+    const StatesInfor = [];
+
+    // Organizando estados por ordem alfabética
+    const States = states.sort((a, b) => {
+      return a.state > b.state ? 1 : b.state > a.state ? -1 : 0;
+    });
+    for (let i = 0; i < States.length; i++) {
+      var id = parseInt(i) + 1;
+      var { uf, state, cases, deaths, suspects, refuses } = States[i];
+      var { latitude, longitude } = coords[i];
+      // Convertendo data e horário
+      var dateTime = new Date(States[i].datetime);
+      var formatDateTime = `${dateTime.getDate()}-${
+        parseInt(dateTime.getMonth()) + 1
+      }-${dateTime.getFullYear()} ${dateTime.getHours()}:${dateTime.getMinutes()}`;
+
+      StatesInfor.push({
+        uf,
+        state,
+        cases,
+        deaths,
+        suspects,
+        refuses,
+        formatDateTime,
+        latitude,
+        longitude,
+      });
+    }
+    setStates(StatesInfor);
   }
   loadsCasesInformation();
   return (
@@ -56,7 +92,7 @@ function Home() {
       <StatusBar barStyle="light-content" backgroundColor="#0f7778" />
       <Header>
         <TextHeader>COVID - 19</TextHeader>
-        <TextHeader>LOGO</TextHeader>
+        <ImageHeader source={logoImg} />
       </Header>
 
       <MapView
@@ -64,25 +100,29 @@ function Home() {
         style={{ flex: 1 }}
         initialRegion={region}
       >
-        <Marker
-          coordinate={{
-            latitude: -12.6583856,
-            longitude: -43.0575209,
-          }}
-        >
-          <Callout>
-            <View style={{ backgroundColor: "#f0f0f5", width: 300 }}>
-              <Text style={{ color: "#333333" }}>{states.state}</Text>
-              <Text style={{ color: "#333333" }}>
-                Casos Confirmados: {states.cases}
-              </Text>
-              <Text style={{ color: "#333333" }}>Mortos: {states.deaths}</Text>
-              <Text style={{ color: "#333333" }}>
-                Atualizado no dia: {states.datetime}
-              </Text>
-            </View>
-          </Callout>
-        </Marker>
+        {states.map((state) => {
+          return (
+            <Marker
+              key={state.uf}
+              coordinate={{
+                latitude: Number(state.latitude),
+                longitude: Number(state.longitude),
+              }}
+            >
+              <ImageMarker source={logoImg} />
+              <Callout>
+                <CalloutContent>
+                  <CalloutText>{state.state}</CalloutText>
+                  <CalloutText>Casos Confirmados: {state.cases}</CalloutText>
+                  <CalloutText>Mortos: {state.deaths}</CalloutText>
+                  <CalloutText>
+                    Atualizado no dia: {state.formatDateTime}
+                  </CalloutText>
+                </CalloutContent>
+              </Callout>
+            </Marker>
+          );
+        })}
       </MapView>
     </Container>
   );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -6,17 +6,31 @@ export const Container = styled.View`
 export const Header = styled.View`
   align-items: center;
   flex-direction: row;
-  background-color: #0f7778;
+  background-color: #f0f0f5;
   justify-content: space-between;
-  padding: 25px 25px 0 25px;
+  padding: 5px 10px 0 10px;
   width: 100%;
 `;
 export const TextHeader = styled.Text`
-  color: #f0f0f5;
+  color: #333;
   font-weight: bold;
 `;
 export const StatusBar = styled.StatusBar``;
-export const CalloutContent = styled.View``;
+export const CalloutContent = styled.View`
+  width: 250px;
+  justify-content: center;
+`;
 export const CalloutText = styled.Text`
-  color: #000;
+  color: #666;
+  font-weight: bold;
+`;
+export const ImageHeader = styled.Image`
+  width: 50px;
+  height: 50px;
+  border-radius: 8px;
+`;
+export const ImageMarker = styled.Image`
+  width: 30px;
+  height: 30px;
+  border-radius: 8px;
 `;
